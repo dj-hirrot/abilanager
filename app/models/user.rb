dfa3ffd983f14a49bb02741a1464fa6738c0acad
dj-hirrot <hirotoshibutani@gmail.com>
2019-11-13 16:05:08 +0900

    [update] バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
     devise :database_authenticatable, :registerable,
            :recoverable, :rememberable, :validatable,
            :confirmable, :trackable
+
+    validates :name, presence: true, length: { minimum: 6, maximum: 15 }, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+    validates :password, presence: true, length: { minimum: 10 }
+    validates :password_confirmation, presence: true, length: { minimum: 10 }
 end
