9fcfb9ffabfaf9ae695b83d418d55d30ff22f6b3
dj-hirrot <hirotoshibutani@gmail.com>
2019-11-13 14:23:12 +0900

    [update] routeの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+  devise_scope :user do
+    get 'join', to: 'users/registrations#new'
+    get 'login', to: 'users/sessions#new'
+    delete 'logout', to: 'users/sessions#destroy'
+  end
   root 'home#top'
 end
